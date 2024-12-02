cbab39c17f0045c46493debecf4715dba860dabe
branislav.popadic@telekom.com <branislav.popadic@telekom.com>
2024-12-02 22:44:11 +0100

    clean and update test

--- utils/mod_test.ts
+++ utils/mod_test.ts
@@ -1,17 +1,17 @@
 import { assertEquals } from "@std/assert";
 import { expect } from "jsr:@std/expect";
-import { getData, getDay, getSolveFn } from "./mod.ts";
+import { getData, getDate, getSolveFn } from "./mod.ts";
 
 Deno.test(function getDayTest() {
-  assertEquals(getDay(), new Date().getDate().toString().padStart(2, "0"));
+  assertEquals(getDate(), {day: new Date().getDate().toString().padStart(2, "0"),year: new Date().getFullYear() + ""});
 });
 
 Deno.test(function getDataTest() {
-  const data = getData("sample");
+  const data = getData('2024',"sample");
   expect(data).toEqual(["1 2 3 4 5", "a b c d e"]);
 });
 
 Deno.test(async function getSolveFnTest() {
-  const solve = await getSolveFn("sample");
+  const solve = await getSolveFn('2024',"sample");
   expect(solve).toBeInstanceOf(Function);
 });
