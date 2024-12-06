6dd3ad963ce3f4593b2761f7d58367a5ef284055
branislav.popadic@telekom.com <branislav.popadic@telekom.com>
2024-12-06 07:34:58 +0100

    day 5

---
+++ 2024/05/mod.ts
@@ -0,0 +1,112 @@
+
+function calculateMiddleValueSum(lists: string[][]): number {
+  return lists.map(u => +u[Math.floor(u.length / 2)]).reduce((a, b) => a + b, 0)
+}
+
+export function part1(data: string) {
+
+  const [rulesRaw, updatesRaw] = data.split('\n\n')
+
+  const rules = rulesRaw.split('\n').map(rule => rule.split('|'))
+  const updates = updatesRaw.split('\n').map(update => update.split(','))
+
+  const correctUpdates: typeof updates = []
+
+  updates.forEach((update) => {
+
+    let updateCorrect = true
+
+    for (let i = 0; i < update.length; i++) {
+      const page = update[i]
+
+      let pageCorrect = true
+
+      const rulesApplied = rules.filter(r => r.includes(page))
+
+
+      rulesApplied.forEach((rule) => {
+        if ((rule[0] === page && update.includes(rule[1]) && !update.slice(i + 1).includes(rule[1]))
+          || (rule[1] === page && update.includes(rule[0]) && !update.slice(0, i).includes(rule[0])))
+          pageCorrect = false
+      });
+
+      if (!pageCorrect)
+        updateCorrect = false
+
+    }
+
+    if (updateCorrect) correctUpdates.push(update)
+
+  })
+
+
+  const middleValueSum = calculateMiddleValueSum(correctUpdates)
+
+  // console.log('correct updates: ', correctUpdates)
+  console.log(middleValueSum)
+
+  return middleValueSum
+}
+
+export function part2(data: string) {
+  const [rulesRaw, updatesRaw] = data.split('\n\n')
+
+  const rules = rulesRaw.split('\n').map(rule => rule.split('|'))
+  const updates = updatesRaw.split('\n').map(update => update.split(','))
+
+  const incorrectUpdates: typeof updates = []
+
+  updates.forEach((update) => {
+
+    let updateCorrect = true
+
+    for (let i = 0; i < update.length; i++) {
+      const page = update[i]
+
+      let pageCorrect = true
+
+      const rulesApplied = rules.filter(r => r.includes(page))
+
+
+      rulesApplied.forEach((rule) => {
+        if ((rule[0] === page && update.includes(rule[1]) && !update.slice(i + 1).includes(rule[1]))
+          || (rule[1] === page && update.includes(rule[0]) && !update.slice(0, i).includes(rule[0])))
+          pageCorrect = false
+      });
+
+      if (!pageCorrect)
+        updateCorrect = false
+
+    }
+
+    if (!updateCorrect) incorrectUpdates.push(update)
+
+  })
+
+  const correctedUpdates = incorrectUpdates.map(update => {
+
+    return update.sort((a, b) => {
+      const testRules = rules.filter(r => r.includes(a) && r.includes(b))
+
+      if (testRules.length === 0) return 0
+
+      return testRules[0].indexOf(a) - testRules[0].indexOf(b)
+
+    })
+
+
+  })
+
+ return calculateMiddleValueSum(correctedUpdates)
+
+}
+
+export default function solve(data: string[]): number {
+
+  const input = data.join('\n')
+
+  console.log(`part1: ${part1(input)}`)
+  console.log(`part2: ${part2(input)}`)
+
+  return 0
+}
---
+++ 2024/05/mod_test.ts
@@ -0,0 +1,39 @@
+import { expect } from 'jsr:@std/expect'
+import { part1, part2 } from './mod.ts'
+
+const testData = `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`
+
+Deno.test(function testPart1Solution() {
+  expect(part1(testData)).toBe(143)
+})
+
+Deno.test(function testPart2Solution() {
+  expect(part2(testData)).toBe(123)
+})
