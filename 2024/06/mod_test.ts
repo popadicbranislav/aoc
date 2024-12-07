6960757a916643c5a350bf7818bd20141a7429d4
branislav.popadic@telekom.com <branislav.popadic@telekom.com>
2024-12-07 22:55:50 +0100

    lint fix

--- 2024/06/mod_test.ts
+++ 2024/06/mod_test.ts
@@ -12,10 +12,10 @@ const sampleData = `....#.....
 #.........
 ......#...`.split("\n").map((r) => r.split(""));
 
-Deno.test(async function solutionPart1Test() {
-  expect(await part1(sampleData)).toBe(41);
+Deno.test(function solutionPart1Test() {
+  expect(part1(sampleData)).toBe(41);
 });
 
-Deno.test(async function solutionPart2Test() {
-  expect(await part2(sampleData)).toBe(6);
+Deno.test(function solutionPart2Test() {
+  expect(part2(sampleData)).toBe(6);
 });
